c7be668cf856b5da5e74ebb77474ed40ae771aae
hy <rlagpdud0524@gmail.com>
2023-06-29 00:42:56 +0900

    back

---
+++ index.js
@@ -0,0 +1,16 @@
+let express=require('express');
+let app=express();
+let path=require('path');
+const cors=require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/',function(res,req){
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+
+
+app.listen(8081,function(){
+    console.log("Server start");
+})
