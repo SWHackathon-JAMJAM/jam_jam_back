f385d2f464db62a158c7cc1ff00a080296fe4c81
hy <rlagpdud0524@gmail.com>
2023-06-30 02:02:42 +0900

    새싹 이미지

--- view/menu.py
+++ view/menu.py
@@ -36,18 +36,39 @@ def info():
     
     res = cur.fetchall()[0]
     if (res[0] is None):
-        exp_tmp = 0
+        exp_tmp = 1
     else:
         exp_tmp = res[0]
+
     if exp_tmp>=1 and exp_tmp < 5: 
-        level = 1
-    elif exp_tmp >= 5 and exp_tmp <10:
         level = 2
-    elif exp_tmp >= 10:
+    elif exp_tmp >= 5 and exp_tmp < 15:
         level = 3
+    elif exp_tmp >= 15 and exp_tmp < 25:
+        level = 4
+    elif exp_tmp >= 25 and exp_tmp < 35:
+        level = 5
+    elif exp_tmp >= 35 and exp_tmp < 45:
+        level = 6
+    elif exp_tmp >= 45 and exp_tmp < 60:
+        level = 7
+    elif exp_tmp >= 60 and exp_tmp < 75:
+        level = 8
+    elif exp_tmp >= 75 and exp_tmp < 90:
+        level = 9
+    elif exp_tmp >= 90:
+        level = 10
     else:
-        level = 0
-    return render_template('info.html', id = mb_id, exp = res[0], level = level)
+        level = 1
+    if level ==1:
+        level_info = 0     
+    elif level>=2 and level<= 5:
+        level_info = 1
+    elif level>=6 and level<= 8:
+        level_info = 2
+    elif level>8:
+        level_info = 3   
+    return render_template('info.html', id = mb_id, exp = res[0], level = level, level_info = level_info)
 
 @menu.route("/level1")
 def level1():
