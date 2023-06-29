8d0f82a95c2ffd800fe91df2f4bd115b319d005f
hy <rlagpdud0524@gmail.com>
2023-06-30 05:57:35 +0900

    게임 구동

--- view/__init__.py
+++ view/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from . import web
-from . import game
+from . import honey_game
 from . import menu
+from . import jam_game
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcd'
 
 app.register_blueprint(menu.menu)
 app.register_blueprint(web.web)
-app.register_blueprint(game.game)
+app.register_blueprint(honey_game.honey_game)
+app.register_blueprint(jam_game.jam_game)
